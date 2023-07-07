c79fdfd8aa0389867f5725ce42526b8db13d9923
AlexDanielGutierrezCardenas <gutierrezcardenasalexdaniel@gmail.com>
2023-07-07 00:01:25 -0400

    feature(common): adding the files of the ticket that was requested in the files [WEBSITE-16]

---
+++ src/ui/components/HistorialPedidos/HistorialPedidos.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Grid,Box, Container, Paper } from '@material-ui/core';
+import ListaPedidos from './ListaPedidos';
+import FiltrarPedidos from './FiltrarPedidos';
+import useStyles from './HistorialPedidos.styles';
+
+
+const HistorialPedidos = () => {
+  const classes = useStyles();
+  return (
+    <Box sx={{display:'flex',width:200}} >
+      <Container className={classes.containersx}>
+        <Grid container spacing={3}>
+          {/*lista pedidos*/}
+          <Grid item>
+            <Paper className={classes.sx}>
+              <ListaPedidos/>
+            </Paper>
+          </Grid>
+          {/*filter*/}
+          <Grid item>
+              <Paper className={classes.xs}>
+                <FiltrarPedidos/>
+              </Paper>
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
+  );
+};
+
+export default HistorialPedidos;
