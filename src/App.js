b69c3ec1b00c68c52c9bdd0e09d9e4eb090d4b3e
yazmintrinidad <trini1010flores@gmail.com>
2023-07-07 14:44:11 -0400

    Adding Client - edit Client

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import Theme from "./Theme/Theme";
 import NavMenu from "./ui/components/navMenu/NavMenu";
 import "bootstrap/dist/css/bootstrap.min.css";
-
 import Cliente from "./Cliente"
 
 function App() {
