e135292624d7686d78e1a62d360278b3ce0a7ad1
yazmintrinidad <trini1010flores@gmail.com>
2023-07-08 09:44:05 -0400

    adding and editing client Websites/15

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import Theme from "./Theme/Theme";
 import NavMenu from "./ui/components/navMenu/NavMenu";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Cliente from "./Cliente"
+
 function App() {
   return (
     <Theme>
-      <NavMenu />
+    <NavMenu />
+   <Cliente/>
     </Theme>
+    
   );
 }
 
-export default App;
+export default App
