b751213e7cc7dffc99c37240517dcafd7293e1aa
Carlos Flores <Carlitos15499@gmail.com>
2023-07-14 11:41:30 -0400

    feature(common): fixed branch conflict to merge [WEBSITES-3]

---
+++ src/ui/components/formClient/FormClient.styles.js
@@ -0,0 +1,69 @@
+import { makeStyles } from "@material-ui/core";
+import { borderRadius } from '@mui/system';
+import mq from "../../../config/mq";
+const useStyles = makeStyles((theme) => ({
+  box: {
+    margin: "0 2rem 0 2rem",
+    justifyContent: "center",
+    display: "flex",
+    [mq("xss")]: {
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    [mq("md")]: {
+      flexDirection: 'row',
+      alignItems: 'stretch',
+    },
+  },
+  formContainer: {
+    width: "100%",
+    margin: "1rem",
+    paddingBottom: "1rem ",
+    border: "1px solid #C0C0C0",
+    borderRadius: '3px'
+  },
+
+  container: {
+    display: "flex",
+    justifyContent: "space-between",
+    padding: "0 1.5rem 0 1.5rem ",
+    gap: 25
+  },
+
+  title: {
+    borderRadius: '5px',
+    backgroundColor: '#EFEFEF',
+    padding: "1rem 0 1rem 1rem",
+    fontSize: "1.5rem ",
+    marginTop: "-0.01rem",
+    fontWeight: 'bold'
+  },
+  formSendDirection: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    [mq("xss")]: {
+      width: '100%',
+    },
+    [mq("md")]: {
+      width: '65%',
+    },
+  },
+  formMark: {
+    marginRight: '2rem',
+    [mq("xss")]: {
+      width: '100%',
+    },
+    [mq("md")]: {
+      width: '35%',
+    },
+  },
+  containerMarketing: {
+    display: "flex",
+    justifyContent: "center",
+    padding: "0 1.5rem 0 1.5rem ",
+    gap: 40
+  }
+
+}));
+export default useStyles;
